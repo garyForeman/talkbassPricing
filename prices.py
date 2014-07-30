272f87c6e1c37ee8d4ee69f0dfe40372fefcb4ee
garyForeman <gforema2@illinois.edu>
2014-07-29 20:50:48 -0500

    Use regex instead of price_parse function

--- prices.py
+++ prices.py
@@ -1,55 +1,70 @@
 #! /usr/bin/env python
-
-################################################################################
-#Author: Gary Foreman
-#Last Modified: April 20, 2014
-#Searches, collects, and plots price data for used basses in the talkbass 
-#classified html pages
-################################################################################
+"""
+Author: Gary Foreman\n
+Last Modified: July 29, 2014\n
+Searches, collects, and plots price data for used basses in the talkbass
+classifieds html pages.
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 import time
 import urllib
-from priceParse import priceParse
+#from price_parse import price_parse
+
 
 NUM_PAGES = 20
 NUM_BINS = 50
-priceList = np.array([])
+MAX_PRICE = 11000
+FLOAT_REGEX = r'\d+,?\d*\.?\d*\s?[kK]?'
 
-for i in xrange(1, NUM_PAGES+1):
-    tbClassifiedPage="http://www.talkbass.com/forums/for-sale-bass-guitars.126/"
-    if i > 1:
-        tbClassifiedPage += "page-" + str(i)
+if __name__ == "__main__":
+    PRICE_LIST = np.array([])
 
-    content = urllib.urlopen(tbClassifiedPage)
-    lines = content.readlines()
-    content.close()
+    for i in xrange(1, NUM_PAGES+1):
+        tb_classified_page = (
+            "http://www.talkbass.com/forums/for-sale-bass-guitars.126/")
+        if i > 1:
+            tb_classified_page += "page-" + str(i)
 
-    for j, line in enumerate(lines):
-        if line.find("Price:") > 0:
-            #html code for line containing price is 
-            #<big><span style="font-weight: bold;">PRICE</span></big>
-            priceString = lines[j+3].split('>')[2].split('<')[0]
+        content = urllib.urlopen(tb_classified_page)
+        lines = content.readlines()
+        content.close()
 
-            try:
-                priceList = np.append(priceList, priceParse(priceString))
-            except IndexError:
-                continue
+        for j, line in enumerate(lines):
+            if line.find("Price:") > 0:
+                #html code for line containing price is
+                #<big><span style="font-weight: bold;">PRICE</span></big>
+                price_string = (
+                    re.findall(FLOAT_REGEX, 
+                               lines[j+3].split('>')[2].split('<')[0]))
 
-print len(priceList)
-print np.mean(priceList)
-print np.std(priceList)
-print np.median(priceList)
-plt.hist(priceList, bins=NUM_BINS, range=(0,10000))
-plt.xticks(np.arange(11)*1000, [str(x) for x in np.arange(11)*1000], 
-           rotation=45, size="xx-large")
-plt.yticks(size="xx-large")
-plt.xlabel("Price ($)", size="xx-large")
-plt.ylabel("Number of basses", size="xx-large")
-plt.title(str(len(priceList)) + " basses, " + time.strftime("%m/%d/%Y"), 
-          size="xx-large")
-plt.text(3000, 20, "median price: $" + str(np.median(priceList)), 
-         size="xx-large")
-plt.savefig("prices_" + time.strftime("%m_%d_%Y") + ".png", bbox_inches="tight",
-            pad_inches=0.05)
+                try:
+                    price_string = price_string[0].replace(',', '')
+                    if(price_string[-1] == 'k' or 
+                       price_string[-1] == 'K'):
+                        price_float = float(price_string[:-1]) * 1000.
+                    else:
+                        price_float = float(price_string)
+
+                    PRICE_LIST = np.append(PRICE_LIST, price_float)
+                except IndexError:
+                    continue
+
+    print len(PRICE_LIST)
+    print np.mean(PRICE_LIST)
+    print np.std(PRICE_LIST)
+    print np.median(PRICE_LIST)
+    plt.hist(PRICE_LIST, bins=NUM_BINS, range=(0, MAX_PRICE))
+    plt.xticks(np.arange(11)*1000, [str(x) for x in np.arange(11)*1000],
+               rotation=45, size="xx-large")
+    plt.yticks(size="xx-large")
+    plt.xlabel("Price ($)", size="xx-large")
+    plt.ylabel("Number of basses", size="xx-large")
+    plt.title(str(len(PRICE_LIST)) + " basses, " + time.strftime("%m/%d/%Y"),
+              size="xx-large")
+    plt.text(3000, 20, "median price: $" + str(np.median(PRICE_LIST)),
+             size="xx-large")
+    plt.savefig("prices_" + time.strftime("%m_%d_%Y") + ".png",
+                bbox_inches="tight", pad_inches=0.05)
