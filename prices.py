92c2d06a078dd479015a2df9921f0131c08066ba
garyForeman <gforema2@illinois.edu>
2014-07-30 16:39:35 -0500

    Updated to use PyQuery

--- prices.py
+++ prices.py
@@ -1,22 +1,21 @@
 #! /usr/bin/env python
 """
 Author: Gary Foreman\n
-Last Modified: July 29, 2014\n
+Last Modified: July 30, 2014\n
 Searches, collects, and plots price data for used basses in the talkbass
 classifieds html pages.
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
+from pyquery import PyQuery as pq
 import re
 import time
-import urllib
-#from price_parse import price_parse
 
 
 NUM_PAGES = 20
-NUM_BINS = 50
-MAX_PRICE = 11000
+NUM_BINS = 60
+MAX_PRICE = 12000
 FLOAT_REGEX = r'\d+,?\d*\.?\d*\s?[kK]?'
 
 if __name__ == "__main__":
@@ -27,37 +26,32 @@ if __name__ == "__main__":
             "http://www.talkbass.com/forums/for-sale-bass-guitars.126/")
         if i > 1:
             tb_classified_page += "page-" + str(i)
+        
+        #initialize PyQuery
+        d = pq(tb_classified_page)
 
-        content = urllib.urlopen(tb_classified_page)
-        lines = content.readlines()
-        content.close()
+        price_strings = d('.pairsInline').find('span').contents()
+        for price_string in price_strings:
+            price_string = re.findall(FLOAT_REGEX, price_string)
+            try:
+                price_string = price_string[0].replace(',', '')
+                if price_string[-1] == 'k' or price_string[-1] == 'K':
+                    price_float = float(price_string[:-1]) * 1000.
+                else:
+                    price_float = float(price_string)
+                
+                PRICE_LIST = np.append(PRICE_LIST, price_float)
 
-        for j, line in enumerate(lines):
-            if line.find("Price:") > 0:
-                #html code for line containing price is
-                #<big><span style="font-weight: bold;">PRICE</span></big>
-                price_string = (
-                    re.findall(FLOAT_REGEX, 
-                               lines[j+3].split('>')[2].split('<')[0]))
-
-                try:
-                    price_string = price_string[0].replace(',', '')
-                    if(price_string[-1] == 'k' or 
-                       price_string[-1] == 'K'):
-                        price_float = float(price_string[:-1]) * 1000.
-                    else:
-                        price_float = float(price_string)
-
-                    PRICE_LIST = np.append(PRICE_LIST, price_float)
-                except IndexError:
-                    continue
+            except IndexError:
+                continue
 
     print len(PRICE_LIST)
     print np.mean(PRICE_LIST)
     print np.std(PRICE_LIST)
     print np.median(PRICE_LIST)
+    print np.max(PRICE_LIST)
     plt.hist(PRICE_LIST, bins=NUM_BINS, range=(0, MAX_PRICE))
-    plt.xticks(np.arange(11)*1000, [str(x) for x in np.arange(11)*1000],
+    plt.xticks(np.arange(13)*1000, [str(x) for x in np.arange(13)*1000],
                rotation=45, size="xx-large")
     plt.yticks(size="xx-large")
     plt.xlabel("Price ($)", size="xx-large")
